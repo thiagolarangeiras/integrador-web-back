41fd3efbdece53c539dafebd36daf2516c8f4316
thiagolarangeiras <thiago.larangeira50@gmail.com>
2025-04-25 17:00:08 -0300

    .

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,9 @@
 ./gradlew build
+
 docker build --build-arg JAR_FILE=build/libs/\*.jar -t integrador_web .
-docker run -d -p 8080:8080 --name integrador_web integrador_web
+
+docker tag integrador_web thiagolarangeira/integrador_web:2
+
+docker push thiagolarangeira/integrador_web:2
+
+docker run -d -p 8080:8080 -e INT_WEB_POSTGRES_PASS --name integrador_web integrador_web

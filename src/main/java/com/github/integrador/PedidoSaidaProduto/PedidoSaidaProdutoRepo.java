b4225c7c20685de6009b5f6528b7990306f49e56
thiagolarangeiras <thiago.larangeira50@gmail.com>
2025-05-05 09:59:34 -0300

    . pdf

--- src/main/java/com/github/integrador/PedidoSaidaProduto/PedidoSaidaProdutoRepo.java
+++ src/main/java/com/github/integrador/PedidoSaidaProduto/PedidoSaidaProdutoRepo.java
@@ -4,8 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PedidoSaidaProdutoRepo extends JpaRepository<PedidoSaidaProduto, Integer> {
-    Optional<Page<PedidoSaidaProduto>> findById(Integer id, Pageable pageable);
+    Optional<Page<PedidoSaidaProduto>> findByIdPedidoSaida(Integer id, Pageable pageable);
+    Optional<List<PedidoSaidaProduto>> findByIdPedidoSaida(Integer id);
 }
